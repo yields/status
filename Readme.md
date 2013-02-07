3317ea7eafe73fe7cc0cc5da45c361b29c06ad8d
Amir Abu Shareb <amir.abu.shareb@me.com>
2013-02-07 19:04:04 +0200

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,45 @@
+
+# status
+
+  User &quot;active&quot; / &quot;idle&quot; event emitter.
+
+## Example
+
+```js
+var status = requrie('status');
+status.watch();
+
+status('idle', function(){
+  console.log('idle!');
+});
+
+status('active', function(){
+  console.log('active!');
+});
+```
+
+## Installation
+
+    $ component install yields/status
+
+## API
+
+### status.watch([timeout])
+
+Start watching on `mousemove` and `keyup` the default `timeout` is 1 minute.
+
+### status.unwatch()
+
+Stop watching `mousemove` and `keyup`.
+
+### status(event, fn)
+
+Invoke `fn` on `event`.
+
+## Todo
+
+  * add tests.
+
+## License
+
+  MIT
---
+++ index.js
@@ -0,0 +1,78 @@
+
+/**
+ * dependencies.
+ */
+
+var emitter = require('emitter')
+  , event = require('event');
+
+/**
+ * flags.
+ */
+
+var tid;
+
+/**
+ * export `status`.
+ */
+
+module.exports = status;
+
+/**
+ * register `fn` to be invoked on `e`.
+ * 
+ * example:
+ * 
+ *      status('active', function(){});
+ *      status('idle', function(){});
+ * 
+ * @param {String} e
+ * @param {Function} fn
+ */
+
+function status(e, fn){
+  status.on(e, fn);
+}
+
+/**
+ * onchange
+ */
+
+status.change = function(){
+  status.idle && status.emit('active');
+  status.idle = null;
+  if (tid) clearTimeout(tid);
+  tid = setTimeout(function(){
+    status.idle = true;
+    status.emit('idle');
+  }, status.timeout);
+};
+
+/**
+ * start watching.
+ */
+
+status.watch = function(timeout){
+  event.bind(window, 'mousemove', status.change, false);
+  event.bind(window, 'keyup', status.change, false);
+  status.timeout = timeout || 6e4;
+  tid = setTimeout(function(){
+    status.idle = true;
+    status.emit('idle');
+  }, status.timeout);
+};
+
+/**
+ * stop watching.
+ */
+
+status.unwatch = function(){
+  event.unbind(window, 'mousemove', status.change);
+  event.unbind(window, 'keyup', status.change);
+};
+
+/**
+ * mixin emitter.
+ */
+
+emitter(status);
